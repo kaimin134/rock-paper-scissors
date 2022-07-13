e9e684a42883949800336f93c2632710c5974c7f
Minh Chung <chungvungocminh@gmail.com>
2022-07-12 23:17:51 -0500

    finish

--- app.js
+++ app.js
@@ -3,5 +3,59 @@ function computerPlay() {
     let computerChoice = Math.floor(Math.random() * choices.length);
     return choices[computerChoice];
 }
-let x = computerPlay();
-console.log(x);
+
+game();
+
+
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Rock") {
+            console.log("Draw! you are both Rock")
+        }
+        else if (computerSelection == "Paper") {
+            console.log("You Lose! Paper beats Rock");
+        }
+        else {
+            console.log("You Win! Rock beats Paper");
+        }
+    }
+    else if (playerSelection == "Paper") {
+        if (computerSelection == "Rock") {
+            console.log("You Win! Paper beats Rock")
+        }
+        else if (computerSelection == "Paper") {
+            console.log("Draw! You are both Paper")
+        }
+        else {
+            console.log("You Lose! Scissors beats Paper")
+        }
+    }
+    else {
+        if (computerSelection == "Rock") {
+            console.log("You Lose! Rock beats Scissors");
+        }
+        else if (computerSelection == "Paper") {
+            console.log("You Win! Scissors beats Paper");
+        }
+        else {
+            console.log("Draw! You are both Scissors")
+        }
+    }
+  }
+
+  function capitalizeFirstLetter (input) {
+      return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase();
+  }
+
+  function game() {
+      for (let i = 0; i < 5; i++) {
+        let x = computerPlay();
+        let input = prompt("Please enter your selection! ");
+        
+        console.log("Computer: " + x);
+        console.log("You: " + capitalizeFirstLetter(input));
+        playRound(capitalizeFirstLetter(input), x);
+
+      }
+  }
